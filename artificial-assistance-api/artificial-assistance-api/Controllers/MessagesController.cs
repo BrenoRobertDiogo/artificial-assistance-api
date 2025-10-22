728fd4a5eb566bf505e61060724ab65688243fbb
Breno R. <brenorobert123@gmail.com>
2025-10-22 12:05:02 -0300

    adicionado providers e servicos

---
+++ artificial-assistance-api/artificial-assistance-api/Controllers/MessagesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace artificial_assistance_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessagesController : ControllerBase
+    {
+        private readonly ILogger<MessagesController> logger;
+        public MessagesController(ILogger<MessagesController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> List()
+        {
+            var retorno = new List<string>();
+            return retorno;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "";
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post()
+        {
+            return "";
+        }
+    }
+}
